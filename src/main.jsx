f763855d8aa23097f26c760a5c8c57023dee9775
Alexander Ruiz <ruizalexander@id.uff.br>
2025-04-16 01:28:26 +0100

    fixies

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,23 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-
-serviceWorkerRegistration.register();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('/sw.js')
+      .then((registration) => {
+        console.log('ServiceWorker registrado com sucesso:', registration.scope);
+      })
+      .catch((error) => {
+        console.error('Falha ao registrar o ServiceWorker:', error);
+      });
+  });
+}
